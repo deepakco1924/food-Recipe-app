ea3d530703e3ea6743d5795dd097d6388bcda2c1
Deepak Pal <co19524@ccet.ac.in>
2022-07-06 21:53:30 +0530

    food recipe app completed

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1954829b-810c-4c4c-9119-11a08e715fc7';
+export const MODAL_CLOSE_SEC = 2.5;
